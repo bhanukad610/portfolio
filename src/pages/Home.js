5721d7cce4f451002a21e4406be27683e78c0c12
bhanukadissanayake <bhanuka.16@cse.mrt.ac.lk>
2024-09-27 23:09:39 +1000

    routing fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../pages/styles/Home.css";
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
@@ -7,12 +8,17 @@ const Home = () => {
       <section className="home">
         <div className="home-content">
           <h1>Hi! I'm Bhanuka</h1>
-          <img src={`${process.env.PUBLIC_URL}/images/home-image.jpg`} alt="Portfolio showcase" className="home-image" /><br></br>
-          <a href={`/about`} className="cta-button">
-            Get to know more about me!
-          </a>
+          <img 
+            src={`${process.env.PUBLIC_URL}/images/home-image.jpg`} 
+            alt="Portfolio showcase" 
+            className="home-image" 
+          />
+          <br />
+          <Link to="/about" className="cta-button">
+          Get to know more about me!
+        </Link>
         </div>
-      </section>
+    </section>
     );
   };
 
