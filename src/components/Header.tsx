d0703ec9c7a844e4906875d6dbcb8aa1921dba97
Bhanuka Dissanayake <31897550+bhanukad610@users.noreply.github.com>
2026-07-09 14:46:00 +0530

    Added Active Nav Link Highlighted (#10)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,21 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import ThemeToggle from './ThemeToggle';
 import './styles/Header.css';
+
+const navLinkClassName = ({ isActive }: { isActive: boolean }) =>
+  isActive ? 'active' : undefined;
 
 const Header = () => {
   return (
     <header>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/resume">Resume</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/blog">Blog</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li><NavLink to="/" end className={navLinkClassName}>Home</NavLink></li>
+          <li><NavLink to="/about" className={navLinkClassName}>About</NavLink></li>
+          <li><NavLink to="/resume" className={navLinkClassName}>Resume</NavLink></li>
+          <li><NavLink to="/projects" className={navLinkClassName}>Projects</NavLink></li>
+          <li><NavLink to="/blog" className={navLinkClassName}>Blog</NavLink></li>
+          <li><NavLink to="/contact" className={navLinkClassName}>Contact</NavLink></li>
         </ul>
         <ThemeToggle />
       </nav>
